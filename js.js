39bb7f115b898c3f9d3b87bdec65eee0fdbf0cca
ivangaston <ivantruccolo@hotmail.com>
2023-06-03 15:17:18 +0200

    avances

--- js.js
+++ js.js
@@ -1,33 +1,46 @@
+/*MENU HAMBURG*/
 const imgMenu = document.getElementById('imgMenu');
 const listaMenu = document.getElementById('listaMenu');
 
 function menuCerrar() {
-  listaMenu.style.opacity = '0';
-  setTimeout(function() {
-    listaMenu.style.display = 'none';
-  }, 500);
+    listaMenu.style.opacity = '0';
+    setTimeout(function () {
+        listaMenu.style.display = 'none';
+    }, 500);
 }
 
-imgMenu.addEventListener('click', function() {
-  if (listaMenu.style.display === 'none') {
-    listaMenu.style.display = 'block';
-    listaMenu.style.opacity = '0';
-    listaMenu.style.transition = 'opacity ease 0.5s';
+imgMenu.addEventListener('click', function () {
+    if (listaMenu.style.display === 'none') {
+        listaMenu.style.display = 'block';
+        listaMenu.style.opacity = '0';
+        listaMenu.style.transition = 'opacity ease 0.5s';
 
-    setTimeout(function() {
-      listaMenu.style.opacity = '1';
-    }, 5);
-  } else {
-    menuCerrar();
-  }
+        setTimeout(function () {
+            listaMenu.style.opacity = '1';
+        }, 5);
+    } else {
+        menuCerrar();
+    }
 });
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     const target = event.target;
     if (target !== imgMenu && !listaMenu.contains(target)) {
-      menuCerrar();
+        menuCerrar();
     }
-  });
-  
-/**************************************************************************/
+});
+
+/*FOOTER*/
+const foot = document.getElementById('mensajes');
+const infoF = document.getElementById('infoF');
+function footCerrar(params) {
+    infoF.style.display = 'none';
+}
+foot.addEventListener('click', () => {
+    if (infoF.style.display === 'none') {
+        infoF.style.display = 'block';
+    } else {
+        footCerrar()
+    }
+})
 
