00b9adf28745ac907d33cc830d6c100295411d40
ivangaston <ivantruccolo@hotmail.com>
2023-06-05 20:07:27 +0200

    avances

--- js.js
+++ js.js
@@ -1,19 +1,19 @@
-/*MENU HAMBURG*/
 const imgMenu = document.getElementById('imgMenu');
 const listaMenu = document.getElementById('listaMenu');
 
+/*MENU HAMBURG*/
 function menuCerrar() {
     listaMenu.style.opacity = '0';
     setTimeout(function () {
         listaMenu.style.display = 'none';
-    }, 500);
+    }, 300);
 }
 
 imgMenu.addEventListener('click', function () {
     if (listaMenu.style.display === 'none') {
         listaMenu.style.display = 'block';
         listaMenu.style.opacity = '0';
-        listaMenu.style.transition = 'opacity ease 0.5s';
+        listaMenu.style.transition = 'opacity ease 0.3s';
 
         setTimeout(function () {
             listaMenu.style.opacity = '1';
@@ -42,5 +42,5 @@ foot.addEventListener('click', () => {
     } else {
         footCerrar()
     }
-})
+});
 
