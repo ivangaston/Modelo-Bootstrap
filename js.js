960469eed14e725665249353382195ad199e3d27
ivangaston <ivantruccolo@hotmail.com>
2023-06-12 18:39:51 +0200

    FINALIZADO

--- js.js
+++ js.js
@@ -1,36 +1,3 @@
-const imgMenu = document.getElementById('imgMenu');
-const listaMenu = document.getElementById('listaMenu');
-
-/*MENU HAMBURG*/
-
-
-function menuAbrir() {
-    listaMenu.style.display = 'block';
-    listaMenu.style.animation = 'opacidad 1s'
-    console.log(menuAbrir)
-}
-
-imgMenu.addEventListener('click', function () {
-    if (listaMenu.style.display === 'block') {
-        setTimeout(function () {
-            listaMenu.style.display = 'none';
-        });
-    } else {
-        menuAbrir();
-    }
-});
-
-function menuCerrar() {
-    listaMenu.style.display = 'none';
-}
-document.addEventListener('click', function (event) {
-    const target = event.target;
-    if (target !== imgMenu && !listaMenu.contains(target)) {
-        menuCerrar();
-    }
-});
-
-
 /*FOOTER*/
 const foot = document.getElementById('mensajes');
 const infoF = document.getElementById('infoF');
