003d28380f982c2eda29abd41e1f8281130529e3
ivangaston <ivantruccolo@hotmail.com>
2023-06-20 17:30:32 +0200

    Finalizado

--- js.js
+++ js.js
@@ -1,18 +1,18 @@
 /*NAVBAR*/
 document.addEventListener('click', (event) => {
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-    const targetElement = event.target;
-    const clickInside = navbarCollapse.contains(targetElement);
-  
-    if (!clickInside) {
-      const navbarToggler = document.querySelector('.navbar-toggler');
-      const navbarOpen = navbarCollapse.classList.contains('show');
-      
-      if (navbarOpen) {
-        navbarToggler.click();
-      }
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+  const targetElement = event.target;
+  const clickInside = navbarCollapse.contains(targetElement);
+
+  if (!clickInside) {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarOpen = navbarCollapse.classList.contains('show');
+
+    if (navbarOpen) {
+      navbarToggler.click();
     }
-  });
+  }
+});
 
 
 /*FOOTER*/
@@ -20,14 +20,14 @@ const foot = document.getElementById('mensajes');
 const infoF = document.getElementById('infoF');
 
 function footAbrir() {
-    infoF.style.display = 'block';
+  infoF.style.display = 'block';
 }
 foot.addEventListener('click', () => {
-    if (infoF.style.display === 'block') {
-        infoF.style.display = 'none';
-    } else {
-        footAbrir()
-    }
+  if (infoF.style.display === 'block') {
+    infoF.style.display = 'none';
+  } else {
+    footAbrir()
+  }
 });
 
 
